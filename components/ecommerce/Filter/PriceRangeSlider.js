4db44af76e07cd49be2c71ffe265d6eeb296c02e
xurshed111 <xurshedistamov005@gmail.com>
2023-07-14 09:08:32 +0300

    first commit

---
+++ components/ecommerce/Filter/PriceRangeSlider.js
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import InputRange from "react-input-range";
+import { connect } from "react-redux";
+import { updateProductFilters } from "../../../redux/action/productFiltersAction";
+
+const PriceRangeSlider = ({ updateProductFilters }) => {
+    // console.log(updateProductFilters);
+
+    const Router = useRouter();
+    const searchTerm = Router.query.search;
+
+    const [price, setPrice] = useState({ value: { min: 0, max: 500 } });
+
+    useEffect(() => {
+        const filters = {
+            price: price.value,
+        };
+
+        updateProductFilters(filters);
+    }, [price, searchTerm]);
+
+    return (
+        <>
+            <InputRange
+                formatLabel={(value) => `$${value}`}
+                maxValue={500}
+                minValue={0}
+                value={price.value}
+                onChange={(value) => setPrice({ value })}
+            />
+
+            
+        </>
+    );
+};
+
+const mapStateToProps = (state) => ({
+    products: state.products.items,
+});
+
+const mapDidpatchToProps = {
+    updateProductFilters,
+};
+
+export default connect(mapStateToProps, mapDidpatchToProps)(PriceRangeSlider);
